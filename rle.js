9e6a8bc41c978ecde934dd60880c6d5e38b6c78c
LitleStuart <mukhorinvanga@gmail.com>
2021-09-30 01:21:26 +0500

    cleaner code and more bug fixes

--- rle.js
+++ rle.js
@@ -1,31 +1,59 @@
 fs = require("fs");
 
-let counter = 1;
-let isRepeatChars = false;
-let isNotRepeatChars = false;
-let newString = "";
+let string = fs.readFileSync(process.argv[3]).toString();
 let notRepeatChars = "";
 
-if (process.argv[2] == "-encode") {
-  var string = fs.readFileSync(process.argv[3]).toString();
+function AddNotRepeatChars(notRepeatChars, newString) {
+  const MAX_UNICODE = 128;
+  let counter = notRepeatChars.length;
+
+  if (counter <= MAX_UNICODE) {
+    newString +=
+      String.fromCharCode(counter + MAX_UNICODE - 1) + notRepeatChars;
+  } else {
+    for (let i = 0; i <= Math.floor(counter / MAX_UNICODE); i++) {
+      let nrc = notRepeatChars.substring(
+        i * MAX_UNICODE,
+        i == Math.floor(counter / MAX_UNICODE)
+          ? (counter % MAX_UNICODE) + MAX_UNICODE * i
+          : i * MAX_UNICODE + MAX_UNICODE
+      );
+      newString += String.fromCharCode(MAX_UNICODE - 1 + nrc.length) + nrc;
+    }
+  }
+  return newString;
+}
+
+function AddRepeatChars(newString, lastChar, counter) {
+  const MAX_UNICODE = 127;
+
+  for (let i = 0; i <= Math.floor(counter / MAX_UNICODE); i++) {
+    if (i == Math.floor(counter / MAX_UNICODE)) {
+      if (counter % MAX_UNICODE == 1) {
+        notRepeatChars += lastChar;
+      } else {
+        newString += String.fromCharCode(counter % MAX_UNICODE) + lastChar;
+      }
+    } else {
+      newString += String.fromCharCode(MAX_UNICODE) + lastChar;
+    }
+  }
+  return newString;
+}
+
+function Encode(string) {
+  let counter = 1;
+  let isRepeatChars = false;
+  let isNotRepeatChars = false;
+  let newString = "";
+
   for (let i = 1; i < string.length; i++) {
     var char = string[i];
     var lastChar = string[i - 1];
     if (char == lastChar) {
       if (isNotRepeatChars) {
-        counter -= 1;
         notRepeatChars = notRepeatChars.substring(0, notRepeatChars.length - 1);
-        if (counter < 129) {
-          newString += String.fromCharCode(counter + 127) + notRepeatChars;
-        } else {
-          for (let i = 0; i <= Math.floor(counter / 128); i++) {
-            let nrc = notRepeatChars.substring(
-              i * 128,
-              i == Math.floor(counter / 128) ? counter % 128 : i * 128 + 128
-            );
-            newString += String.fromCharCode(127 + nrc.length) + nrc;
-          }
-        }
+        newString = AddNotRepeatChars(notRepeatChars, newString);
         counter = 1;
       }
       isNotRepeatChars = false;
@@ -34,85 +62,56 @@ if (process.argv[2] == "-encode") {
       counter += 1;
     } else {
       if (isRepeatChars) {
-        if (counter < 128) {
-          newString += String.fromCharCode(counter) + lastChar;
-        } else {
-          newString += String.fromCharCode(127) + lastChar;
-          if (counter == 128) {
-            notRepeatChars += lastChar;
-          } else {
-            for (let i = 1; i <= Math.floor(counter / 127); i++) {
-              if (i == Math.floor(counter / 127)) {
-                if (counter % 127 == 1) {
-                  notRepeatChars += lastChar;
-                } else {
-                  newString += String.fromCharCode(counter % 127) + lastChar;
-                }
-              } else {
-                newString += String.fromCharCode(127) + lastChar;
-              }
-            }
-          }
-        }
-        isRepeatChars = false;
+        newString = AddRepeatChars(newString, lastChar, counter);
         notRepeatChars += char;
         counter = notRepeatChars.length;
         if (counter == 2) {
           isNotRepeatChars = true;
+        } else {
+          isRepeatChars = false;
         }
       } else {
         isNotRepeatChars = true;
-        counter += 1;
+        if (i == 1) {
+          notRepeatChars += lastChar;
+        }
         notRepeatChars += char;
       }
     }
   }
-  if (isNotRepeatChars) {
-    counter = notRepeatChars.length;
-    if (counter < 129) {
-      newString += String.fromCharCode(counter + 127) + notRepeatChars;
-    } else {
-      for (let i = 0; i <= Math.floor(counter / 128); i++) {
-        let nrc = notRepeatChars.substring(
-          i * 128,
-          i == Math.floor(counter / 128) ? counter % 128 : i * 128 + 128
-        );
-        newString += String.fromCharCode(127 + nrc.length) + nrc;
-      }
-    }
-  } else if (isRepeatChars) {
-    if (counter < 128) {
-      newString += String.fromCharCode(counter) + lastChar;
-    } else {
-      for (let i = 0; i <= Math.floor(counter / 127); i++) {
-        if (i < Math.floor(counter / 127)) {
-          newString += String.fromCharCode(127) + lastChar;
-        } else {
-          newString += String.fromCharCode(counter % 127) + lastChar;
-        }
-      }
-    }
+  if (isRepeatChars) {
+    newString = AddRepeatChars(newString, lastChar, counter);
   }
-
-  console.log(newString);
-  fs.writeFileSync(process.argv[4], newString);
+  if (notRepeatChars != "") {
+    newString = AddNotRepeatChars(notRepeatChars, newString);
+  }
+  return newString;
 }
 
-if (process.argv[2] == "-decode") {
-  var string = fs.readFileSync(process.argv[3]).toString();
+function Decode(string) {
+  const MAX_UNICODE = 127;
+  let newString = "";
+
   for (let i = 0; i < string.length; i++) {
     let index = string.charCodeAt(i);
-    if (index < 128) {
+    if (index <= MAX_UNICODE) {
       newString += string[i + 1].repeat(index);
       i++;
     } else {
-      index -= 127;
+      index -= MAX_UNICODE;
       newString += string.substring(i + 1, i + index + 1);
       i += index;
     }
   }
-  console.log(newString);
-  fs.writeFileSync(process.argv[4], newString);
+  return newString;
+}
+
+if (process.argv[2] == "-encode") {
+  fs.writeFileSync(process.argv[4], Encode(string));
+}
+
+if (process.argv[2] == "-decode") {
+  fs.writeFileSync(process.argv[4], Decode(string));
 }
 
 if (process.argv[2] == "-help") {
