9e49b98ff8fa62c3f20266ba8c15a6b11875120c
LitleStuart <mukhorinvanga@gmail.com>
2021-10-15 19:50:09 +0500

    try/catch added

--- rle.js
+++ rle.js
@@ -3,14 +3,18 @@ const { decodeEscape } = require("./decodeEscape");
 const { encodeEscape } = require("./encodeEscape");
 const { serializeString } = require("./serializeString");
 
-const str = fs.readFileSync(process.argv[3]).toString();
+try {
+    const str = fs.readFileSync(process.argv[3]).toString();
 
-if (process.argv[2] == "encode") {
-    const encodedStr = encodeEscape(serializeString(str));
-    fs.writeFileSync(process.argv[4], encodedStr);
-}
-
-if (process.argv[2] == "decode") {
-    const decodedStr = decodeEscape(str);
-    fs.writeFileSync(process.argv[4], decodedStr);
+    if (process.argv[2] == "code") {
+        const encodedStr = encodeEscape(serializeString(str));
+        fs.writeFileSync(process.argv[4], encodedStr);
+    } else if (process.argv[2] == "decode") {
+        const decodedStr = decodeEscape(str);
+        fs.writeFileSync(process.argv[4], decodedStr);
+    } else {
+        console.log("Input code or decode");
+    }
+} catch {
+    console.log("Incorrect input/output file");
 }
