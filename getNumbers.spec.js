a97b59f3694ad5c2406c2b412c0c7da4b7fe08a8
LitleStuart <mukhorinvanga@gmail.com>
2021-10-15 17:24:14 +0500

    added test

--- getNumbers.spec.js
+++ getNumbers.spec.js
@@ -1,12 +1,17 @@
 const { getNumbers } = require("./getNumbers");
 describe("getNumbers", () => {
-  it("Returns array of numbers", () => {
-    const result = getNumbers(100, 1);
-    expect(result).toEqual([1]);
-  });
+    it("Returns array of numbers", () => {
+        const result = getNumbers(100, 1);
+        expect(result).toEqual([1]);
+    });
 
-  it("Returns array of numbers", () => {
-    const result = getNumbers(100, 100);
-    expect(result).toEqual([100]);
-  });
+    it("Returns array of numbers", () => {
+        const result = getNumbers(100, 100);
+        expect(result).toEqual([100]);
+    });
+
+    it("Returns array of numbers", () => {
+        const result = getNumbers(100, 201);
+        expect(result).toEqual([100, 100, 1]);
+    });
 });
