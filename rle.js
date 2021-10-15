53b61ca6b34de57087378fe75d84ad9b90cabad8
LitleStuart <mukhorinvanga@gmail.com>
2021-10-15 18:50:09 +0500

    fix bug

--- rle.js
+++ rle.js
@@ -1,15 +1,16 @@
 const fs = require("fs");
 const { decodeEscape } = require("./decodeEscape");
 const { encodeEscape } = require("./encodeEscape");
+const { serializeString } = require("./serializeString");
 
 const str = fs.readFileSync(process.argv[3]).toString();
 
 if (process.argv[2] == "encode") {
-  const encodedStr = encodeEscape(serializeString(str));
-  fs.writeFileSync(process.argv[4], encodedStr);
+    const encodedStr = encodeEscape(serializeString(str));
+    fs.writeFileSync(process.argv[4], encodedStr);
 }
 
 if (process.argv[2] == "decode") {
-  const decodedStr = decodeEscape(str);
-  fs.writeFileSync(process.argv[4], decodedStr);
+    const decodedStr = decodeEscape(str);
+    fs.writeFileSync(process.argv[4], decodedStr);
 }
